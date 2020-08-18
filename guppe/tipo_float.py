4adc66641a2733493131983a89d727acf1a62aff
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-08-18 10:10:34 -0300

    18/08/2020

--- guppe/tipo_float.py
+++ guppe/tipo_float.py
@@ -2,5 +2,36 @@
 Tipo float
 Tipo real, decimal
 Casas decimais
+
+Obs.: o separador de casas decimais na programação é o ponto e não a vírgula.
 """
 
+# Errado do ponto de vista do float mas gera uma tupla:
+valor = 1, 44
+print(valor)
+print(type(valor))
+
+# Certo do ponto de vista float:
+valor = 1.44
+print(valor)
+print(type(valor))
+
+# É possível fazer dupla artibuição:
+valor1, valor2 = 1, 44
+print(f'Valor 1: {valor1}')
+print(type(valor1))
+print(f'Valor 2: {valor2}')
+print(type(valor2))
+
+# Podemos converter um float para um int
+"""
+Ao converter valores float para inteiro nos perdemos precisão.
+"""
+res = int(valor)
+print(f'Resultado: {res}')
+print(type(res))
+
+# Podemos trabalhar com números complexos.
+variavel = 5j
+print(variavel)
+print(type(variavel))
--- guppe/tipo_numerico.py
+++ guppe/tipo_numerico.py
@@ -1,3 +1,10 @@
 """
 Tipo numérico
 """
+
+# Podemos separar as casas por underline
+num = 1_000_000
+print(f'Inteiro: {num}')
+
+# Possível converter interio para float
+print(f'Convertido: {float(num)}')
